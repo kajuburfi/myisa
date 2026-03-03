b62d7d13635fc67227b2244033a35530c1ae9158
kajuburfi <82087036+kajuburfi@users.noreply.github.com>
2026-03-03 15:24:04 +0530

    Isolate `load_pgm_into_mem()` into a standalone function.

--- computer.c
+++ computer.c
@@ -1,34 +1,46 @@
 #include <stdio.h>
 
+// Global memory and registers
 short int mem[65536] = {0};
 short int reg[16] = {0};
 
-void load_pgm_into_mem(FILE *fp) {
-  char buf[2];
-  short int idx = 0xFFFF;
+// Reads through the file pointer, and loads the pgm into the memory
+// Returns the end of the program portion.
+short load_pgm_into_mem(char *name) {
+  FILE *fp = fopen(name, "rb");
+  if (!fp)
+    return -1;
+
+  char buf[2];            // We read as chars
+  short int idx = 0xFFFF; // Index to store into
+  // Reading loop
   while (fread(buf, sizeof(buf), 1, fp) == 1) {
+    // I want to store it as short int(16 bits)
     short val = (((short)buf[0]) << 8) | (0x00FF & buf[1]);
     mem[idx--] = val;
     // The below line prints the pgm in mem
     printf("%X %04X\n", idx & 0x0000FFFF, val & 0x0000FFFF);
   }
-  printf("Bottom line of pgm is %x", idx);
+  // printf("Bottom line of pgm is %x", idx);
+  fclose(fp);
+
+  return idx;
 }
 
 void set_up_regs() { reg[0xA] = 0xFFFF; }
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
-    printf("Error: please provide machine code binary file.");
+    printf("Error: please provide machine code binary file.\n");
     return 1;
   }
 
-  FILE *fp = fopen(argv[1], "rb");
-
-  load_pgm_into_mem(fp);
-
-  fclose(fp);
-
+  set_up_regs();
+  short pgm_end = load_pgm_into_mem(argv[1]);
+  if (pgm_end < 0) {
+    printf("Error: Couldn't open binary provided\n");
+    return 1;
+  }
   return 0;
 }
 
